16446d84bcd1bca19417f820745edb1f65e1fca6
RT Flores <rotyflo@gmail.com>
2019-01-22 15:36:45 -0600

    Enable user to add events before today

--- sanitize.py
+++ sanitize.py
@@ -20,57 +20,39 @@ def test_usr(msg):
 def get_year():
     """Returns desired year in the form of an integer."""
     while True:
-        year = input('\nEnter in Year xxxx: ')
+        year = input('\nYear: ')
 
-        if year.isdigit():
-            year = int(year)
+        if year.isdigit(): return int(year)
 
-            if year >= current_year:
-                return year
-            else:
-                print(f'Year must be greater than {current_year - 1}')
-        else:
-            print(val_err)
+        else: print(val_err)
 
 def get_month(year):
     """Returns desired month in the form of an integer."""
     while True:
-        month = input('\nEnter in month: ')
+        month = input('\nMonth: ')
 
         if month.isdigit():
             month = int(month)
 
-            if year == current_year:
-                if month in range(current_month, 13):
-                    return month
-                else:
-                    print(f'Month must be between {current_month} and 12.')
-            elif month in range(1, 13):
-                return month
-            else:
-                print(f'Month must be between 1 and 12.')
-        else:
-            print(val_err)
+            if month in range(1, 13): return month
+            
+            else: print(f'Month must be between 1 and 12.')
+        
+        else: print(val_err)
 
 def get_day(year, month):
     """Returns desired day in the form of an integer."""
     while True:
-        day = input('\nEnter in day: ')
+        day = input('\nDay: ')
 
         if day.isdigit():
             day = int(day)
 
-            if year == current_year and month == current_month:
-                if day in range(current_day, 32):
-                    return day
-                else:
-                    print(f'Day must be between {current_day} and 31.')
-            elif day in range(1, 32):
-                return day
-            else:
-                print(f'Day must be between 1 and 31.')
-        else:
-            print(val_err)
+            if day in range(1, 32): return day
+            
+            else: print(f'Day must be between 1 and 31.')
+        
+        else: print(val_err)
 
 
 def sani_date():
@@ -86,32 +68,30 @@ def sani_date():
 def get_hour():
     """Returns desired hour in the form of an integer."""
     while True:
-        hour = input('\nEnter in hour: ')
+        hour = input('\nHour: ')
         
         if hour.isdigit():
             hour = int(hour)
 
-            if hour in range(24):
-                return hour
-            else:
-                print(f'Hour must be between 0 and 23.')
-        else:
-            print(val_err)
+            if hour in range(24): return hour
+            
+            else: print(f'Hour must be between 0 and 23.')
+        
+        else: print(val_err)
 
 def get_minute():
     """Returns desired minute in the form of an integer."""
     while True:
-        minute = input('\nEnter in minute: ')
+        minute = input('\nMinute: ')
         
         if minute.isdigit():
             minute = int(minute)
 
-            if minute in range(60):
-                return minute
-            else:
-                print(f'Minute must be between 0 and 59.')
-        else:
-            print(val_err)
+            if minute in range(60): return minute
+            
+            else: print(f'Minute must be between 0 and 59.')
+        
+        else: print(val_err)
 
 def sani_time():
     """Returns a valid time string to be interpreted as a datetime object"""
