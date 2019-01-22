324a6787ddfd07f6c672492a019cd4067ac6c8a9
RT Flores <rotyflo@gmail.com>
2019-01-22 10:31:55 -0600

    Add methods to event object

--- event.py
+++ event.py
@@ -1,7 +1,14 @@
-from sanitize import sani_date
-
-class Event(object):
+class Event():
     """An event with datetime features """
-    def __init__(self):
-        self.date = sani_date()
-        self.time = sani_date()
+    def __init__(self, date, time):
+        self.date = date
+        self.time = time
+
+    def print_event_details(self):
+        print(f'\nEvent scheduled for {self.date} at {self.time}.')
+
+    def get_date(self):
+        return self.date
+
+    def get_time(self):
+        return self.time
