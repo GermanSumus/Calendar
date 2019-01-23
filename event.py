120e9da85b392a08e585d9fc9d1e09f3da4c59df
RT Flores <rotyflo@gmail.com>
2019-01-22 18:47:41 -0600

    Format date object in method found in 'Event'

--- event.py
+++ event.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Event():
     """An event with datetime features """
     def __init__(self, date, time):
@@ -5,7 +7,9 @@ class Event():
         self.time = time
 
     def print_event_details(self):
-        print(f'\nEvent scheduled for {self.date} at {self.time}.')
+        formatted_date = self.date.strftime('%A, %B %d, %Y')
+        
+        print(f'\nEvent scheduled for {formatted_date} at {self.time}.')
 
     def get_date(self):
         return self.date
