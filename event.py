e55fdbc568add508ebe310849e9789ffcd6ff377
German's Laptop <GermanRodriguezWork@gmail.com>
2019-01-23 05:04:50 -0600

    Move method

--- event.py
+++ event.py
@@ -1,7 +1,11 @@
-from sanitize import sani_date
+from datetime import timedelta
+from sanitize import sani_date, sani_time
 
 class Event(object):
     """An event with datetime features """
     def __init__(self):
         self.date = sani_date()
-        self.time = sani_date()
+#        self.time = sani_time()
+
+    def move(self, week=0, day=0, hour=0, minute=0):
+        self.date = self.date + timedelta(weeks=week, days=day, hours=hour, minutes=minute)
