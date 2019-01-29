b2ee678e4db9f55592014ad350ab13e92c092156
German Rodriguez <GermanRodriguezWork@Gmail.com>
2019-01-29 08:59:19 -0600

    writes to cal.dict file our event objs

--- evaluate_calendar.py
+++ evaluate_calendar.py
@@ -10,3 +10,9 @@ def read_cal(cal_file_path='cal.dict'):
         finally:
             with open(cal_file_path) as file:
                 return eval(file.read())
+
+def write_to_cal(event, cal_file_path='cal.dict'):
+    cal = read_cal()
+    cal[event.datetime.year][event.datetime.month][event.datetime.day].append('New Event')
+    with open(cal_file_path, 'w') as file:
+        file.write(cal.__repr__())
