91c06efc0dfad1b5ea2612bfbaf0f89dd6f4a2e7
German Rodriguez <GermanRodriguezWork@Gmail.com>
2019-02-04 15:09:22 -0600

    json compatible

--- schedule_prints.py
+++ schedule_prints.py
@@ -2,12 +2,6 @@
 weekly_schedule():
 Return the weeks schedule in a human readable by reading our calendar.
 A week is seven days from today.
-Our calendars are going to be in a txt file that will evaluate to:
-    a dict containing        years as keys and calendar as a value.
-    calendar is a dict of    months as keys and its days as a value.
-    days is a list of        Event_objects or an empty list.
-example: {2019: {1: {1:[], 2:[], ..} }, {2: {1:[], 2:[], ..}, ..},
-          2020: {1: {1:[], 2:[], ..} }, {2: {1:[], 2:[], ..}, ..}}
 """
 import datetime
 from evaluate_calendar import read_cal
@@ -16,14 +10,9 @@ from manipulate_cal import make_year
 def weekly_schedule(cal_file_path='cal.dict', now=datetime.datetime.now()):
     """Print out the weeks schedule for humans to read"""
     cal = read_cal()
-
     if len(cal) < 1: cal = make_year()
-
-    #print(f'\n{now} \n***DAYS***\n{now + datetime.timedelta(days=6)}\n')
     print("\nPREVIEW OF THE WEEK")
-    # BUG IN THE NEXT LINES BELOW. DATE CANT GO PAST 1/31/2019.
     for x in range(7):
         date = now + datetime.timedelta(days=x)
-        day_events = cal[date.year][date.month][date.day]
-
+        day_events = cal[str(date.year)][str(date.month)][str(date.day)]
         print('\t', date.strftime('%B %d, %Y'), day_events)
