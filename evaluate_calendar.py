91c411f35d6fdc035853c5f45c1439e9b033ffa3
German Rodriguez <GermanRodriguezWork@Gmail.com>
2019-02-04 15:07:07 -0600

    uses json for storing structured data and reading

--- evaluate_calendar.py
+++ evaluate_calendar.py
@@ -1,7 +1,12 @@
+from json import load, dump
 
 def read_cal(cal_file_path='cal.dict'):
     """Reads the cal.dict file and returns an empty dictionary at least"""
     with open(cal_file_path) as file:
+        try:
+            return load(file, parse_int=True)
+        except:
+            return {}
         try:
             return eval(file.read())
         except SyntaxError:
@@ -13,6 +18,6 @@ def read_cal(cal_file_path='cal.dict'):
 
 def write_to_cal(event, cal_file_path='cal.dict'):
     cal = read_cal()
-    cal[event.datetime.year][event.datetime.month][event.datetime.day].append('New Event')
+    cal[event.year][event.month][str(event.datetime.day)][event.time]= event.description
     with open(cal_file_path, 'w') as file:
-        file.write(cal.__repr__())
+        dump(cal, file, indent=2)
