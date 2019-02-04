1c12801f55812547dff6366f8c7724394e4c6447
German Rodriguez <GermanRodriguezWork@Gmail.com>
2019-02-04 15:08:34 -0600

    using json and recurision to read file again

--- manipulate_cal.py
+++ manipulate_cal.py
@@ -1,7 +1,10 @@
 import datetime
+from json import dump
+
 from sanitize import test_usr, get_datetime_obj, description
 from event import Event
-from evaluate_calendar import write_to_cal
+from evaluate_calendar import write_to_cal, read_cal
+
 
 def make_year(year=datetime.datetime.now().year):
     """This returns a yearly calendar as shown above"""
@@ -9,18 +12,18 @@ def make_year(year=datetime.datetime.now().year):
     thirty_one = [1,3,5,7,8,10,12]
     for month in range(1,13):
         if month in thirty_one:
-            days = dict(zip([x for x in range(1,32)], [[] for x in range(1,32)]))
+            days = dict(zip([x for x in range(1,32)], [{} for x in range(1,32)]))
             cal[year][month] = days
         elif month == 2:
-            days = dict(zip([x for x in range(1,29)], [[] for x in range(1,29)]))
+            days = dict(zip([x for x in range(1,29)], [{} for x in range(1,29)]))
             cal[year][month] = days
         else:
-            days = dict(zip([x for x in range(1,31)], [[] for x in range(1,31)]))
+            days = dict(zip([x for x in range(1,31)], [{} for x in range(1,31)]))
             cal[year][month] = days
 
     with open('cal.dict', 'w') as file:
-        file.write(cal.__repr__())
-    return cal
+        dump(cal, file, indent=2)
+    return read_cal()
 
 def add_event():
     if test_usr('\nNeed to add something?'):
